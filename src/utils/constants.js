3743bb896e455399c2646d7c240adf2c782f0767
Smith Zhang <smithzhang0427@gmail.com>
2022-04-22 14:50:18 +0800

    update to mainnet

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -32,26 +32,26 @@ export const CONTRACTS_TYPE = {
 };
 
 export const CONTRACTS = {
-  
+
   [CONTRACTS_TYPE.SCARDUST_TOKEN]: {
-    1: { address: '', abi: '' },
+    1: { address: '0x8b561b023580fbe6eee234c8d1079d5c7e828393', abi: scarDustToken.abi },
     4: { address: "0x131107D25b125eAB1A8685113Cb32fBddb2B5042", abi: scarDustToken.abi },
     56: { address: '', abi: '' },
     97: { address: '0x131107D25b125eAB1A8685113Cb32fBddb2B5042', abi: scarDustToken.abi },
   },
   [CONTRACTS_TYPE.TOKEN_DISTRIBUTOR]: {
-    1: { address: '', abi: '' },
+    1: { address: '0xda5f1423f9c996B6FEAD44c95BaDF38FdD8bdF3e', abi: tokenDistributor.abi },
     4: { address: "0x799175C454413283ed09e875118b062b4D0ec489", abi: tokenDistributor.abi },
     56: { address: '', abi: '' },
     97: { address: '0x799175C454413283ed09e875118b062b4D0ec489', abi: tokenDistributor.abi },
   },
   [CONTRACTS_TYPE.FEESHARING_SYSTEM]: {
-    1: { address: '', abi: '' },
+    1: { address: '0x38daCCb1e6B3A232295296e275584fa7c8EE06d6', abi: feeSharingSystem.abi },
     4: { address: "0x60f31CA91319d1d03710aF52eC5461422ce80EDF", abi: feeSharingSystem.abi },
     56: { address: '', abi: '' },
     97: { address: '0x60f31CA91319d1d03710aF52eC5461422ce80EDF', abi: feeSharingSystem.abi },
   },
 
-  
+
 };
 
